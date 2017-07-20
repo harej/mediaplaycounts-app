409c4365d967d207645b3664c9a018c97dc973ba
harej <jamesmhare@gmail.com>
2017-07-19 21:35:25 -0700

    New version: 2
    1) Cleaned up a lot of duplicate code
    2) Removed the AskCommons methods
    3) The output should be more consistent and less duplicative now,
       per the changes made in the mediaplaycounts library.

--- app.py
+++ app.py
@@ -1,20 +1,57 @@
 from flask import Flask, request, make_response, Response
-from mediaplaycounts.MediaPlaycounts.GetData.FilePlaycount import date as fpc_date, date_range as fpc_date_range, last_30 as fpc_last_30, last_90 as fpc_last_90
-from mediaplaycounts.MediaPlaycounts.GetData.CategoryPlaycount import date as cpc_date, date_range as cpc_date_range, last_30 as cpc_last_30, last_90 as cpc_last_90
-from mediaplaycounts.MediaPlaycounts.GetData.AskCommons import find_subcategories, find_media_files
+import mediaplaycounts.MediaPlaycounts.GetData as mpc
 import json
-
-# I've been doing too much JavaScript
-true = True
-false = False
 
 app = Flask(__name__)
 
 def _api_response(payload):
-    resp = make_response(json.dumps(payload))
+    resp = make_response(json.dumps(payload, indent=4))
     resp.headers["Content-Type"] = "application/json"
     return resp
 
+def _doc_response(payload):
+    resp = {'parameters': []}
+
+    mapping = {
+        'filename':
+            {"@label": "filename",
+             "@notes": "Do not include the 'File:' prefix.",
+             "type": "string",
+             "required": True},
+        'date':
+            {"@label": "date",
+             "@notes": "Format must be YYYYMMDD",
+             "type": "string",
+             "required": True},
+        'start_date':
+            {"@label": "start_date",
+             "@notes": "Format must be YYYYMMDD and must be chronologically earlier than end_date",
+             "type": "string",
+             "required": True},
+        'end_date':
+            {"@label": "end_date",
+             "@notes": "Format must be YYYYMMDD and must be chronologically later than start_date",
+             "type": "string",
+             "required": True},
+        'category':
+            {"@label": "category",
+             "@notes": "Do not include the 'Category:' prefix.",
+             "type": "string",
+             "required": True},
+        'depth':
+            {"@label": "depth",
+             "@notes": "Level of subcategory traversal",
+             "type": "number",
+             "required": False,
+             "default": 9}
+    }
+
+    for thing in payload:
+        resp['parameters'].append(mapping[thing])
+
+    return _api_response(resp)
+
+### Informational ###
 
 @app.route('/')
 def home():
@@ -22,260 +59,116 @@ def home():
 
 @app.route('/api')
 def apihome():
-    info = {"current_version": 1, "all_versions": [1]}
+    info = {"current_version": 2, "all_versions": [2]}
     return _api_response(info)
 
-@app.route('/api/1')
+@app.route('/api/2')
 def list_current_methods():
-    info = {"methods": ["FilePlaycount", "CategoryPlaycount", "AskCommons"]}
+    info = {"methods": ['file_playcount', 'category_playcount']}
     return _api_response(info)
 
-@app.route('/api/1/FilePlaycount')
-def fileplaycount_directory():
+@app.route('/api/2/file_playcount')
+def file_playcount_directory():
     info = {"methods": ["date", "date_range", "last_30", "last_90"]}
     return _api_response(info)
 
-@app.route('/api/1/CategoryPlaycount')
-def categoryplaycount_directory():
+@app.route('/api/2/category_playcount')
+def category_playcount_directory():
     info = {"methods": ["date", "date_range", "last_30", "last_90"]}
     return _api_response(info)
 
-@app.route('/api/1/AskCommons')
-def askcommons_directory():
-    info = {"methods": ["find_subcategories", "find_media_files"]}
+@app.route('/api/2/file_playcount/date')
+def file_playcount_date_doc():
+    return _doc_response(['filename', 'date'])
+
+@app.route('/api/2/file_playcount/date_range')
+def file_playcount_date_range_doc():
+    return _doc_response(['filename', 'start_date', 'end_date'])
+
+@app.route('/api/2/file_playcount/last_30')
+def file_playcount_last_30_doc():
+    return _doc_response(['filename'])
+
+@app.route('/api/2/file_playcount/last_90')
+def file_playcount_last_90_doc():
+    return _doc_response(['filename'])
+
+@app.route('/api/2/category_playcount/date')
+def category_playcount_date_doc():
+    return _doc_response(['category', 'date', 'depth'])
+
+@app.route('/api/2/category_playcount/date_range')
+def category_playcount_date_range_doc():
+    return _doc_response(['category', 'start_date', 'end_date', 'depth'])
+
+@app.route('/api/2/category_playcount/last_30')
+def category_playcount_last_30_doc():
+    return _doc_response(['category', 'depth'])
+
+@app.route('/api/2/category_playcount/last_90')
+def category_playcount_last_90_doc():
+    return _doc_response(['category', 'depth'])
+
+### File Playcount ###
+
+@app.route('/api/2/file_playcount/date/<filename>/<date>')
+def file_playcount_date(filename, date):
+    info = mpc.file_playcount(filename, end_date=date)
     return _api_response(info)
 
-@app.route('/api/1/FilePlaycount/date')
-def fileplaycount_date_doc():
-    info = {"parameters": [
-                            {"@label": "filename",
-                             "@notes": "Do not include the 'File:' prefix.",
-                             "type": "string",
-                             "required": true},
-                            {"@label": "date",
-                             "@notes": "Format must be YYYYMMDD",
-                             "type": "string",
-                             "required": true}
-                          ]
-           }
+@app.route('/api/2/file_playcount/date_range/<filename>/<start_date>/<end_date>')
+def file_playcount_date_range(filename, start_date, end_date):
+    info = mpc.file_playcount(filename, start_date=start_date, end_date=end_date)
     return _api_response(info)
 
-@app.route('/api/1/FilePlaycount/date/<filename>/<date>')
-def fileplaycount_date(filename, date):
-    info = fpc_date(filename, date)
+@app.route('/api/2/file_playcount/last_30/<filename>')
+def file_playcount_last_30(filename):
+    info = mpc.file_playcount(filename, last=30)
     return _api_response(info)
 
-@app.route('/api/1/FilePlaycount/date_range')
-def fileplaycount_date_range_doc():
-    info = {"parameters": [
-                            {"@label": "filename",
-                             "@notes": "Do not include the 'File:' prefix.",
-                             "type": "string",
-                             "required": true},
-                            {"@label": "start_date",
-                             "@notes": "Format must be YYYYMMDD and must be chronologically earlier than end_date",
-                             "type": "string",
-                             "required": true},
-                            {"@label": "end_date",
-                             "@notes": "Format must be YYYYMMDD and must be chronologically later than start_date",
-                             "type": "string",
-                             "required": true}
-                          ]
-           }
+@app.route('/api/2/file_playcount/last_90/<filename>')
+def file_playcount_last_90(filename):
+    info = mpc.file_playcount(filename, last=90)
     return _api_response(info)
 
-@app.route('/api/1/FilePlaycount/date_range/<filename>/<start_date>/<end_date>')
-def fileplaycount_date_range(filename, start_date, end_date):
-    info = fpc_date_range(filename, start_date, end_date)
+### Category Playcount ###
+
+@app.route('/api/2/category_playcount/date/<category>/<date>')
+def category_playcount_date_autodepth(category, date):
+    info = mpc.category_playcount(category, end_date=date)
     return _api_response(info)
 
-@app.route('/api/1/FilePlaycount/last_30')
-def fileplaycount_last_30_doc():
-    info = {"parameters": [
-                            {"@label": "filename",
-                             "@notes": "Do not include the 'File:' prefix.",
-                             "type": "string",
-                             "required": true}
-                          ]
-           }
+@app.route('/api/2/category_playcount/date/<category>/<date>/<depth>')
+def category_playcount_date_userdepth(category, date, depth):
+    info = mpc.category_playcount(category, depth=int(depth), end_date=date)
     return _api_response(info)
 
-@app.route('/api/1/FilePlaycount/last_30/<filename>')
-def fileplaycount_last_30(filename):
-    info = fpc_last_30(filename)
+@app.route('/api/2/category_playcount/date_range/<category>/<start_date>/<end_date>')
+def category_playcount_date_range_autodepth(category, start_date, end_date):
+    info = mpc.category_playcount(category, start_date=start_date, end_date=end_date)
     return _api_response(info)
 
-@app.route('/api/1/FilePlaycount/last_90')
-def fileplaycount_last_90_doc():
-    info = {"parameters": [
-                            {"@label": "filename",
-                             "@notes": "Do not include the 'File:' prefix.",
-                             "type": "string",
-                             "required": true}
-                          ]
-           }
+@app.route('/api/2/category_playcount/date_range/<category>/<start_date>/<end_date>/<depth>')
+def category_playcount_date_range_userdepth(category, start_date, end_date, depth):
+    info = mpc.category_playcount(category, depth=int(depth), start_date=start_date, end_date=end_date)
     return _api_response(info)
 
-@app.route('/api/1/FilePlaycount/last_90/<filename>')
-def fileplaycount_last_90(filename):
-    info = fpc_last_90(filename)
+@app.route('/api/2/category_playcount/last_30/<category>')
+def category_playcount_last_30_autodepth(category):
+    info = mpc.category_playcount(category, last=30)
     return _api_response(info)
 
-@app.route('/api/1/CategoryPlaycount/date')
-def categoryplaycount_date_doc():
-    info = {"parameters": [
-                            {"@label": "category",
-                             "@notes": "Do not include the 'Category:' prefix.",
-                             "type": "string",
-                             "required": true},
-                            {"@label": "date",
-                             "@notes": "Format must be YYYYMMDD",
-                             "type": "string",
-                             "required": true},
-                            {"@label": "depth",
-                             "@notes": "Level of subcategory traversal",
-                             "type": "number",
-                             "required": false,
-                             "default": 9}
-                          ]
-           }
+@app.route('/api/2/category_playcount/last_30/<category>/<depth>')
+def category_playcount_last_30_userdepth(category, depth):
+    info = mpc.category_playcount(category, depth=depth, last=30)
     return _api_response(info)
 
-@app.route('/api/1/CategoryPlaycount/date/<category>/<date>')
-def categoryplaycount_date_autodepth(category, date):
-    info = cpc_date(category, date)
+@app.route('/api/2/category_playcount/last_90/<category>')
+def category_playcount_last_90_autodepth(category):
+    info = mpc.category_playcount(category, last=90)
     return _api_response(info)
 
-@app.route('/api/1/CategoryPlaycount/date/<category>/<date>/<depth>')
-def categoryplaycount_date_userdepth(category, date, depth):
-    info = cpc_date(category, date, depth=int(depth))
+@app.route('/api/2/category_playcount/last_90/<category>/<depth>')
+def category_playcount_last_90_userdepth(category, depth):
+    info = mpc.category_playcount(category, depth=depth, last=90)
     return _api_response(info)
-
-@app.route('/api/1/CategoryPlaycount/date_range')
-def categoryplaycount_date_range_doc():
-    info = {"parameters": [
-                            {"@label": "category",
-                             "@notes": "Do not include the 'Category:' prefix.",
-                             "type": "string",
-                             "required": true},
-                            {"@label": "start_date",
-                             "@notes": "Format must be YYYYMMDD and must be chronologically earlier than end_date",
-                             "type": "string",
-                             "required": true},
-                            {"@label": "end_date",
-                             "@notes": "Format must be YYYYMMDD and must be chronologically later than start_date",
-                             "type": "string",
-                             "required": true},
-                            {"@label": "depth",
-                             "@notes": "Level of subcategory traversal",
-                             "type": "number",
-                             "required": false,
-                             "default": 9}
-                          ]
-           }
-    return _api_response(info)
-
-@app.route('/api/1/CategoryPlaycount/date_range/<category>/<start_date>/<end_date>')
-def categoryplaycount_date_range_autodepth(category, start_date, end_date):
-    info = cpc_date_range(category, start_date, end_date)
-    return _api_response(info)
-
-@app.route('/api/1/CategoryPlaycount/date_range/<category>/<start_date>/<end_date>/<depth>')
-def categoryplaycount_date_range_userdepth(category, start_date, end_date, depth):
-    info = cpc_date_range(category, start_date, end_date, depth=int(depth))
-    return _api_response(info)
-
-@app.route('/api/1/CategoryPlaycount/last_30')
-def categoryplaycount_last_30_doc():
-    info = {"parameters": [
-                            {"@label": "category",
-                             "@notes": "Do not include the 'Category:' prefix.",
-                             "type": "string",
-                             "required": true},
-                            {"@label": "depth",
-                             "@notes": "Level of subcategory traversal",
-                             "type": "number",
-                             "required": false,
-                             "default": 9}
-                          ]
-           }
-    return _api_response(info)
-
-@app.route('/api/1/CategoryPlaycount/last_30/<category>')
-def categoryplaycount_last_30_autodepth(category):
-    info = cpc_last_30(category)
-    return _api_response(info)
-
-@app.route('/api/1/CategoryPlaycount/last_30/<category>/<depth>')
-def categoryplaycount_last_30_userdepth(category, depth):
-    info = cpc_last_30(category, depth=int(depth))
-    return _api_response(info)
-
-@app.route('/api/1/CategoryPlaycount/last_90')
-def categoryplaycount_last_90_doc():
-    info = {"parameters": [
-                            {"@label": "category",
-                             "@notes": "Do not include the 'Category:' prefix.",
-                             "type": "string",
-                             "required": true},
-                            {"@label": "depth",
-                             "@notes": "Level of subcategory traversal",
-                             "type": "number",
-                             "required": false,
-                             "default": 9}
-                          ]
-           }
-    return _api_response(info)
-
-@app.route('/api/1/CategoryPlaycount/last_90/<category>')
-def categoryplaycount_last_90_autodepth(category):
-    info = cpc_last_90(category)
-    return _api_response(info)
-
-@app.route('/api/1/CategoryPlaycount/last_90/<category>/<depth>')
-def categoryplaycount_last_90_userdepth(category, depth):
-    info = cpc_last_90(category, depth=int(depth))
-    return _api_response(info)
-
-@app.route('/api/1/AskCommons/find_subcategories')
-def askcommons_find_subcategories_doc():
-    info = {"parameters": [
-                            {"@label": "category",
-                             "@notes": "Do not include the 'Category:' prefix.",
-                             "type": "string",
-                             "required": true},
-                            {"@label": "depth",
-                             "@notes": "Level of subcategory traversal",
-                             "type": "number",
-                             "required": false,
-                             "default": 9}
-                          ]
-           }
-    return _api_response(info)
-
-@app.route('/api/1/AskCommons/find_subcategories/<category>')
-def askcommons_find_subcategories_autodepth(category):
-    info = find_subcategories(category)
-    return _api_response(info)
-
-@app.route('/api/1/AskCommons/find_subcategories/<category>/<depth>')
-def askcommons_find_subcategories_userdepth(category, depth):
-    info = find_subcategories(category, depth=int(depth))
-    return _api_response(info)
-
-@app.route('/api/1/AskCommons/find_media_files')
-def askcommons_find_media_files_doc():
-    info = {"parameters": [
-                            {"@label": "category",
-                             "@notes": "Do not include the 'Category:' prefix.",
-                             "type": "string",
-                             "required": true}
-                          ]
-           }
-    return _api_response(info)
-
-@app.route('/api/1/AskCommons/find_media_files/<category>')
-def askcommons_find_media_files(category):
-    info = find_media_files(category)
-    return _api_response(info)
-
